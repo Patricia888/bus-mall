f0d031665931eccb528479544ca7267830de2e18
Patricia G Raftery <praftery8@gmail.com>
2018-01-22 15:14:29 -0800

    3 random image generator working

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function Images(filepath, name) {
   this.name = name;
   Images.allBusMallImages.push(this);
 }
-
+console.log(Images.allBusMallImages);
 
 //create instances of Busmall Pics
 new Images('img/bag.jpg', 'R2D2 bag');
@@ -43,18 +43,33 @@ var imgEl3 = document.getElementById('busmallpic3');
 
 //event listener on the image
 imgEl.addEventListener('click', randomImage);
+imgEl2.addEventListener('click', randomImage);
+imgEl3.addEventListener('click', randomImage);
 
 
 //callback function for the event listener (to randomly display a goat image)
 function randomImage() {
   //random number generator to return a number between 0 and the length of the array (Goat.allGoats)
-  var randomIndex = Math.floor(Math.random() * Goat.allGoats.lenghth);
+  var randomIndex = Math.floor(Math.random() * Images.allBusMallImages.length);
 
 
   //use the random number to display a goat at that random index
   imgEl.src = Images.allBusMallImages[randomIndex].filepath;
 }
 
+function randomImage2() {
+  var randomIndex = Math.floor(Math.random() * Images.allBusMallImages.length);
+
+  imgEl2.src = Images.allBusMallImages[randomIndex].filepath;
+}
+
+function randomImages3() {
+  var randomIndex = Math.floor(Math.random() * Images.allBusMallImages.length);
+
+  imgEl3.src = Images.allBusMallImages[randomIndex].filepath;
+}
 
 //invoke the callback (on page load to show a random baby goat)
 randomImage();
+randomImage2();
+randomImages3();
