956cc45ffde4d0d743d9fb0326ff08da393cb619
Patricia G Raftery <praftery8@gmail.com>
2018-01-23 13:37:36 -0800

    deleted unneeded h3s in html, wrote vote tracker in js

--- app.js
+++ app.js
@@ -19,87 +19,123 @@ var productVotes = [];
 
 
 //make a constructor function for Images objects
-function Images(filepath, name) {
-  this.filepath = filepath;
+function Images(name, filepath) {
   this.name = name;
+  this.filepath = filepath;
+  this.votes = 0;
+  this.timesDisplayed = 0;
   Images.allBusMallImages.push(this);
+  imageNames.push(this.name);
 }
 console.log(Images.allBusMallImages);
 
 //create instances of Busmall Pics
-new Images('img/bag.jpg', 'R2D2 bag');
-new Images('img/banana.jpg', 'banana cutter');
-new Images('img/bathroom.jpg', 'bathroom ipad stand');
-new Images('img/boots.jpg', 'rainboots with holes');
-new Images('img/breakfast.jpg', 'tiny breaky cooker');
-new Images('img/bubblegum.jpg', 'horrible gum');
-new Images('img/chair.jpg', 'rounded chair');
-new Images('img/cthulhu.jpg', 'cthulhu monster figure');
-new Images('img/dog-duck.jpg', 'duck bill for dogs');
-new Images('img/dragon.jpg', 'dragon meat');
-new Images('img/pen.jpg', 'silverware pens');
-new Images('img/pet-sweep.jpg', 'pet foot brooms');
-new Images('img/scissors.jpg', 'pizza scissors');
-new Images('img/shark.jpg', 'terrible shark sleeping bag');
-new Images('img/sweep.png', 'babby sweep outfit');
-new Images('img/tauntaun.jpg', 'tauntaun sleeping bag');
-new Images('img/unicorn.jpg', 'unicorn meat');
-new Images('img/usb.gif', 'tentacle usb');
-new Images('img/water-can.jpg', 'looped water can');
-new Images('img/wine-glass.jpg', 'wine glass');
+new Images('R2D2 bag', 'img/bag.jpg');
+new Images('banana cutter', 'img/banana.jpg');
+new Images('bathroom ipad stand', 'img/bathroom.jpg');
+new Images('rainboots with holes', 'img/boots.jpg');
+new Images('tiny breaky cooker', 'img/breakfast.jpg');
+new Images('horrible gum', 'img/bubblegum.jpg');
+new Images('rounded chair', 'img/chair.jpg');
+new Images('cthulhu monster figure', 'img/cthulhu.jpg');
+new Images('duck bill for dogs', 'img/dog-duck.jpg');
+new Images('dragon meat', 'img/dragon.jpg');
+new Images('silverware pens', 'img/pen.jpg');
+new Images('pet foot brooms', 'img/pet-sweep.jpg');
+new Images('pizza scissors', 'img/scissors.jpg');
+new Images('terrible shark sleeping bag', 'img/shark.jpg');
+new Images('babby sweep outfit', 'img/sweep.png');
+new Images('tauntaun sleeping bag', 'img/tauntaun.jpg');
+new Images('unicorn meat', 'img/unicorn.jpg');
+new Images('tentacle usb', 'img/usb.gif');
+new Images('looped water can', 'img/water-can.jpg');
+new Images('wine glass', 'img/wine-glass.jpg');
 
 
 //access the image from the DOM
 //imgEl is image element
-var imgEl = document.getElementById('busmallpic1');
+var imgEl1 = document.getElementById('busmallpic1');
 var imgEl2 = document.getElementById('busmallpic2');
 var imgEl3 = document.getElementById('busmallpic3');
 
 
-//event listener on the image
-imgEl.addEventListener('click', allRandomImages);
-imgEl2.addEventListener('click', allRandomImages);
-imgEl3.addEventListener('click', allRandomImages);
-
-
-//callback function for the event listener (to randomly display a goat image)
+//randomly display one of the pictures
 function allRandomImages() {
-  function randomImage() {
-  //random number generator to return a number between 0 and the length of the array (Goat.allGoats)
-    var randomIndex = Math.floor(Math.random() * Images.allBusMallImages.length);
-    //use the random number to display a goat at that random index
-    imgEl.src = Images.allBusMallImages[randomIndex].filepath;
-    numberOfClicks1++;
-    console.log('1st pic clicks: ' + numberOfClicks1);
+  var randomImage1 = Math.floor(Math.random() * Goat.allGoats.length);
+  var randomImage2 = Math.floor(Math.random() * Goat.allGoats.length);
+  var randomImage3 = Math.floor(Math.random() * Goat.allGoats.length);
+
+  //check to make sure each random number is unique
+  //if same, generate new random numbers
+  //condition 1: 1 and 2 are the same                 check
+  //condition 2: 1 and 3 are the same                 check
+  //condition 3: 2 and 3 are the same                 check
+  //condition 4: 1 is in the lastDisplayed array      check
+  //condition 5: 2 is in the lastDisplayed array      check
+  //condition 6: 3 is in the lastDisplayed array
+  while (randomImage1 === randomImage2 || randomImage1 === randomImage3 || randomImage2 === randomImage3 || Images.lastDisplayed.includes(randomImage1) || Images.lastDisplayed.includes(randomImage2) || Images.lastDisplayed.includes(randomImage3)) {
+    console.log('Duplicate was caught');
+
+    randomImage1 = Math.floor(Math.random() * Goat.allGoats.length);
+    randomImage2 = Math.floor(Math.random() * Goat.allGoats.length);
+    randomImage3 = Math.floor(Math.random() * Goat.allGoats.length);
   }
-  function randomImage2() {
-    var randomIndex = Math.floor(Math.random() * Images.allBusMallImages.length);
+  //set the src and alt attributes
+  imgEl1.src = Images.allBusMallImages[randomImage1].filepath;
+  imgEl1.alt = Images.allBusMallImages[randomImage1].name;
+  imgEl2.src = Images.allBusMallImages[randomImage2].filepath;
+  imgEl2.alt = Images.allBusMallImages[randomImage2].name;
+  imgEl3.src = Images.allBusMallImages[randomImage3].filepath;
+  imgEl3.alt = Images.allBusMallImages[randomImage3].name;
 
-    imgEl2.src = Images.allBusMallImages[randomIndex].filepath;
-    numberOfClicks2++;
-    console.log('2nd pic clicks: ' + numberOfClicks2);
+  //increment the number of times EACH image was shown
+  Images.allBusMallImages[randomImage1].timesDisplayed += 1;
+  Images.allBusMallImages[randomImage2].timesDisplayed += 1;
+  Images.allBusMallImages[randomImage3].timesDisplayed += 1;
+
+  //keep track of previously displayed
+  Images.lastDisplayed[0] = randomImage1;
+  Images.lastDisplayed[1] = randomImage2;
+  Images.lastDisplayed[2] = randomImage3;
+}
+
+//define handleClick function
+function handleClick(event) {
+  //track total number of clicks
+  Images.totalNumberOfClicks += 1;
+  console.log(event.target.alt);
+  //count clicks on a specific image
+  for (var i in Images.allBusMallImages) {
+    if (event.target.alt === Images.allBusMallImages[i].name) {
+      Images.allBusMallImages[i].votes += 1;
+    }
   }
-  function randomImage3() {
-    var randomIndex = Math.floor(Math.random() * Images.allBusMallImages.length);
-
-    imgEl3.src = Images.allBusMallImages[randomIndex].filepath;
-    numberOfClicks3++;
-    console.log('3rd pic clicks: ' + numberOfClicks2);
-  }
-  randomImage();
-  randomImage2();
-  randomImage3();
-
-  totalNumberOfClicks = numberOfClicks1 + numberOfClicks2 + numberOfClicks3;
-
-  console.log(totalNumberOfClicks);
-
-  if (totalNumberOfClicks > 25) {
-    alert('Thank you for your participation! Here are your results.');
+  if (Images.totalNumberOfClicks > 25) {
+    sectionEl.removeEventListener('click', handleClick);
+    showResults();
+    updateVotes();
+    renderChart();
+  } else {
+    allRandomImages();
   }
 }
 
-//invoke the callback
+function showResults() {
+  for (var i in Images.allBusMallImages) {
+    var liEl = document.createElement('li)');
+    liEl.textContent = Images.allBusMallImages[i].name + ' has ' + Images.allBusMallImages[i].votes + ' votes and was displayed ' + Images.allBusMallImages[i].timesDisplayed + ' times.'
+    ulEl.appendChild(liEl);
+  }
+}
+
+//function to update the number of votes per product
+function updateVotes() {
+  for(var i in Images.allBusMallImages) {
+    productVotes[i] = Images.allBusMallImages[i].votes;
+  }
+}
+
+sectionEl.addEventListener('click', handleClick);
 
 
 allRandomImages();
